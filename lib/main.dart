8107261d5b8df4b70c635dcb3b2dab623171950d
lesterholsterr <mattyang98@gmail.com>
2022-06-12 16:51:52 -0400

    Final product (hopefully) minus windows alarm and speech to text integration

--- lib/main.dart
+++ lib/main.dart
@@ -3,37 +3,206 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ZoomAFK());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+String name = "";
+
+class ZoomAFK extends StatelessWidget {
+  ZoomAFK({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MyHomePage(
-        title: 'Makria'
+      title: 'Navigation Demo',
+      theme: ThemeData(
+        fontFamily: 'VarelaRound',
+        primarySwatch: Colors.blue,
       ),
+      home: const FirstPage(title: 'Welcome'),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 50.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Image(
+              image: AssetImage('assets/Makria-removebg-preview.png'),
+            ),
+          ),
+          Container(
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const SecondPage(title: "Enter Your Details");
+                  }));
+                },
+                child: const Text("Start"),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  primary: Colors.black,
+                  side: BorderSide(color: Colors.black),
+                  minimumSize: Size(120, 80),
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 50.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Enter Your First Name',
+            style: TextStyle(
+              fontSize: 50.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.black,
+            ),
+          ),
+          Center(
+            child: Padding(
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(15),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'You Must Enter Your First Name',
+                          hintText: 'Enter Your First Name',
+                        ),
+                        onChanged: (String text) {
+                          name = text;
+                          print('Your First Name is $name');
+                        },
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                          return const MyApp();
+                        }));
+                  },
+                  child: const Text("Become Makria"),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    primary: Colors.black,
+                    side: BorderSide(color: Colors.black),
+                    minimumSize: Size(120, 80),
+                  ),
+                ),
+              ],
+            ),
+            /*child: TextButton(
+              onPressed: () {},
+              child: const Text("Go to Afk Page"),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.white,
+                primary: Colors.black,
+                side: BorderSide(color: Colors.black),
+                minimumSize: Size(120, 80),
+              ),
+            )*/
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Align(
+          alignment: Alignment.bottomCenter,
+          child: TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Go to back to second page"))),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(title: 'Makria'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
-
   String imageLink = 'assets/off_button.png';
   int status = 0; //0 = off, 1 = on, 2 = on and alert
-  final player = AudioCache();
 
   void _onButtonPressed(String value) {
     setState(() {
@@ -42,7 +211,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _onAlert() async {
-    player.play('kid_air_raid.mp3');
+    final player = AudioCache();
+    player.load('kid_air_raid.mp3');
     _onButtonPressed('assets/alert_button.png');
     status = 2;
   }
@@ -61,31 +231,41 @@ class _MyHomePageState extends State<MyHomePage> {
               icon: Image.asset(imageLink),
               iconSize: 500,
               onPressed: () {
-                if (status == 0){
+                if (status == 0) {
                   _onButtonPressed('assets/on_button.png');
                   status = 1;
-                }
-                else if(status == 1){
+                } else if (status == 1) {
                   _onButtonPressed('assets/off_button.png');
                   status = 0;
-                }
-                else{
-                  _onButtonPressed('assets/on_button.png');
-                  status = 1;
-                  player.clearAll();
                 }
               },
             ),
             FloatingActionButton(
               onPressed: () {
-                if(status == 1){
+                if (status == 1) {
                   _onAlert();
-                  Future.delayed(const Duration(seconds: 13), (){
+                  Future.delayed(const Duration(seconds: 13), () {
                     _onButtonPressed('assets/on_button.png');
                     status = 1;
                   });
-                }
-                else null;
+                } else
+                  null;
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.white,
+                primary: Colors.black,
+                side: BorderSide(color: Colors.black),
+                minimumSize: Size(120, 80),
+              ),
+              child: const Text(
+                'Rename',
+              ),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const SecondPage(title: "Enter Your Details");
+                }));
               },
             ),
           ],
@@ -93,5 +273,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-
 }
